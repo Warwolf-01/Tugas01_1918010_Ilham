777c726e2af45003083c7d75ab03cb30374a53b1
WARWOLF_108th <ilhambm191@example.com>
2021-10-10 00:10:34 +0700

    first commit

---
+++ app/src/main/java/com/example/tugas01_1918010/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.tugas01_1918010;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Biodata(View view) {
+        Intent a = new Intent(MainActivity.this, Biodata.class);
+        startActivity(a);
+
+    }
+
+    public void segitiga(View view){
+        Intent b = new Intent(MainActivity.this,
+                segitiga.class);
+        startActivity(b);
+    }
+
+}
